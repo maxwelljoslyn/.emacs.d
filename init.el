2a6f84e6261468c75ce894d5326520b62ed978d9
Maxwell Joslyn <joslynm@orpa488.ohsu.edu>
2017-10-31 13:50:02 -0700

    define todo keywords and their quick keys within init file

--- init.el
+++ init.el
@@ -63,6 +63,10 @@
   (setq org-default-notes-file "~/Desktop/todo.org")
   (setq org-log-into-drawer t)
 
+  (setq org-todo-keywords
+	'((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)" "CANCELLED(c)")
+	  (sequence "WAIT(w)" "HOLD(h)" "|" "DONE(d)" "CANCELLED(c)")))
+
   (setq org-todo-keyword-faces
       (quote (("TODO" :foreground "red" :weight bold)
               ("NEXT" :foreground "deep sky blue" :weight bold)
