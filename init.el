50afad9113f2439699273ec5e68d6be50da24f10
Maxwell Joslyn <joslynm@orpa488.ohsu.edu>
2017-11-09 11:13:43 -0800

    fix keybinding for truncate-lines

--- init.el
+++ init.el
@@ -56,7 +56,7 @@
 
   (define-key org-mode-map (kbd "C-M-RET") 'org-insert-subheading)
   (define-key org-mode-map (kbd "C-'") nil)
-  (define-key org-mode-map (kbd "\M-q") 'toggle-truncate-lines)
+  (define-key org-mode-map (kbd "M-q") 'toggle-truncate-lines)
   (define-key org-mode-map (kbd "C-c t") 'org-todo)
   
   (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
