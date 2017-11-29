f7e23ef1abbd92d31599b5b8b83a31385f40d282
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2017-11-29 10:32:40 -0800

    fix erroneous binding of M-k

--- init.el
+++ init.el
@@ -241,7 +241,7 @@
 (global-set-key (kbd "M-j") 'backward-word)
 (global-set-key (kbd "M-l") 'forward-word)
 (global-set-key (kbd "M-i") 'previous-line)
-(global-set-key (kbd "M-k") 'forward-line)
+(global-set-key (kbd "M-k") 'next-line)
 (define-key helm-map (kbd "M-i") 'helm-previous-line)
 (define-key helm-map (kbd "M-k") 'helm-next-line)
 
