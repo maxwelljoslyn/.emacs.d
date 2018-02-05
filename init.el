1f9440bb7ff07da99559f5860d6003d1a2a31df3
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2018-02-05 15:29:58 -0800

    add function to insert a filename

--- init.el
+++ init.el
@@ -167,6 +167,26 @@ Derived from Norang setup."
 	     :config
 	     (which-key-mode))
 
+(defun mj/insert-filename (filename &optional args)
+    "Insert path to file FILENAME into buffer after point.
+  
+  Prefixed with \\[universal-argument], expand the file name to
+  its fully canonicalized path.  See `expand-file-name'.
+  
+  Prefixed with \\[negative-argument], use relative path to file
+  name from current directory, `default-directory'.  See
+  `file-relative-name'.
+  
+  The default with no prefix is to insert the file name exactly as
+  it appears in the minibuffer prompt."
+    (interactive "*fInsert file name: \nP")
+    (cond ((eq '- args)
+           (insert (file-relative-name filename)))
+          ((not (null args))
+           (insert (expand-file-name filename)))
+          (t
+           (insert filename))))
+
 (use-package ess
   :ensure t)
 
