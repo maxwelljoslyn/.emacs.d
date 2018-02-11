f2b069b12f89cf290c5ed3a461466873282e03b4
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2018-02-11 12:43:57 -0800

    update favorite files

--- init.el
+++ init.el
@@ -492,7 +492,7 @@
 ;; find favorites unless they're already visited
 ;; this stops Emacs from switching over to that file if I'm just evaling my whole init.el while tweaking it
 
-(let ((favorite-files '("~/Desktop/todo.org" "~/.emacs.d/init.el" "/Users/maxwelljoslyn/Desktop/projects/finance.ledger" "~/Desktop/projects/D&D/master_file.org"))
+(let ((favorite-files '("~/Desktop/todo.org" "~/.emacs.d/init.el" "/Users/maxwelljoslyn/Desktop/projects/finance.ledger" "~/Desktop/projects/D&D/campaign/masterfile.org" "~/Desktop/projects/site/index.org"))
             value)		;make sure list starts empty
   (dolist (element favorite-files value)
     (unless (get-file-buffer element)
