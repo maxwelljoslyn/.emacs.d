c035a79d8adac7d4089b0577b228e652f68335ef
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2018-05-07 10:42:36 -0700

    close customize paren
    For some reason, the sexp written by customize was unbalanced. Woops.

--- init.el
+++ init.el
@@ -781,4 +781,4 @@ version 2016-12-18"
  ;; If there is more than one, they won't work right.
  '(dired-marked ((t (:foreground "tan3")))))
 (put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil))
