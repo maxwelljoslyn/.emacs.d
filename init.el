12e6a4974f824d42272c59847511431e7e08f949
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2018-06-29 12:58:27 -0700

    put Customize settings into their own file

--- init.el
+++ init.el
@@ -692,22 +692,6 @@ This is the format Ledger requires."
 ;; (add-to-list 'load-path "~/.emacs.d/evil")
 ;; (require 'evil)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("935cdfc778539529d8124a5500923a660b7e05eb9dba5a762107c7db7a4d56ae" default)))
- '(package-selected-packages
-   (quote
-    (seoul256-theme yasnippet which-key web-mode wc-goal-mode visual-regexp-steroids use-package undo-tree swoop swiper-helm smartparens slime sass-mode paredit nand2tetris multiple-cursors markdown-mode magit keyfreq iedit helm-swoop haskell-mode goto-chg flycheck expand-region exec-path-from-shell ess el-get dumb-jump dired-subtree dired-filter define-word csv-mode corral company cider auctex ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(dired-marked ((t (:foreground "tan3"))))
- '(outline-1 ((t (:inherit default :foreground "#81a2ff"))))
- '(font-lock-warning-face ((t (:inherit default :foreground "red3")))))
+(setq custom-file "~/.emacs.d/custom.el")
+(when (file-exists-p custom-file)
+  (load custom-file))
