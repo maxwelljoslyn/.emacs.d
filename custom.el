9a606930a57ad94dacfbdd33ffddde660a4c2877
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2018-10-05 10:06:31 -0700

    add my custom file
    I can't believe I forgot to have this committed. Woops. Maybe it
    really is time for an emacs bankruptcy.

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("935cdfc778539529d8124a5500923a660b7e05eb9dba5a762107c7db7a4d56ae" default)))
+ '(package-selected-packages
+   (quote
+    (seoul256-theme yasnippet which-key web-mode wc-goal-mode visual-regexp-steroids use-package undo-tree swoop swiper-helm smartparens slime sass-mode paredit nand2tetris multiple-cursors markdown-mode magit keyfreq iedit helm-swoop haskell-mode goto-chg flycheck expand-region exec-path-from-shell ess el-get dumb-jump dired-subtree dired-filter define-word csv-mode corral company cider auctex ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dired-marked ((t (:foreground "tan3"))))
+ '(font-lock-warning-face ((t (:inherit default :foreground "red2"))))
+ '(org-block ((t (:inherit shadow :foreground "orange"))))
+ '(outline-1 ((t (:inherit default :foreground "#81a2ff")))))
