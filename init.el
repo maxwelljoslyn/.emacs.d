4834cdfcffb62f20274195bba2280d14926f741c
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2018-11-09 16:10:48 -0800

    fix a typo
    Oops, crap, this is why I should have tests for my init file. Ah, the
    work never ends.

--- init.el
+++ init.el
@@ -448,7 +448,7 @@ Derived from Norang setup."
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
 ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
 ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
-(global-set-key (kbd "C-c h")# 'helm-command-prefix)
+(global-set-key (kbd "C-c h") #'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
 (global-set-key (kbd "M-x") #'helm-M-x)
