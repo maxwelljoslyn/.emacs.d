5c7a95a32e3f076bd33a458cc3b187abf2b6d5d8
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2019-01-04 14:40:42 -0800

    change ansi faces so directories in terminal emulator are readable

--- custom.el
+++ custom.el
@@ -3,6 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "green3" "yellow3" "DarkOrchid2" "magenta3" "cyan3" "gray90"])
  '(custom-safe-themes
    (quote
     ("935cdfc778539529d8124a5500923a660b7e05eb9dba5a762107c7db7a4d56ae" default)))
